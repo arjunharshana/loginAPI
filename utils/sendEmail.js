7f1e545a10530679cf2b15a7e3e8b74e5ec3bb10
arjunharshana <arjunharshana11@gmail.com>
2025-10-08 11:51:49 +0530

    Add function to send otp

---
+++ utils/sendEmail.js
@@ -0,0 +1,20 @@
+const transporter = require("../config/email");
+
+const sendEmail = async (email, otp) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "Your OTP Code",
+    text: `Your OTP code is ${otp}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
+};
+
+module.exports = sendEmail;
