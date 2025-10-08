6b25d7c3de13c741945a206ee89cdad794b1fd4f
arjunharshana <arjunharshana11@gmail.com>
2025-10-08 14:49:09 +0530

    Add login function

--- controllers/authcontrol.js
+++ controllers/authcontrol.js
@@ -83,3 +83,42 @@ verifyOtp = async (request, response) => {
     response.status(500).json({ message: "Server error" });
   }
 };
+
+login = async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    if (!email || !password) {
+      return response.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return response.status(400).json({ message: "Invalid credentials" });
+    }
+
+    if (!user.isVerified) {
+      return response.status(400).json({ message: "Email not verified" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return response.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id, username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "3h" }
+    );
+
+    response.status(200).json({
+      message: "Login successful",
+      token,
+      user: { id: user._id, username: user.username, email: user.email },
+    });
+  } catch (error) {
+    response.status(500).json({ message: "Server error" });
+  }
+};
